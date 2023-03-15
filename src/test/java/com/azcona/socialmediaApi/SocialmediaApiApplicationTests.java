9b1d53c24804ba0fe91aebcc7d1430c22e7cc3e7
marcelo-azcona <marceloazcona86@gmail.com>
2023-03-15 19:58:32 -0300

    Uploading Boot starting files

---
+++ src/test/java/com/azcona/socialmediaApi/SocialmediaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azcona.socialmediaApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialmediaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
