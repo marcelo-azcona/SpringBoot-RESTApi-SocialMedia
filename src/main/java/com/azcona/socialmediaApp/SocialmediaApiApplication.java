25ddc52ed650d0d8d68e60abc48178e5e98b9549
marcelo-azcona <marceloazcona86@gmail.com>
2023-03-15 20:20:37 -0300

    Creating the User class

--- src/main/java/com/azcona/socialmediaApp/SocialmediaApiApplication.java
+++ src/main/java/com/azcona/socialmediaApp/SocialmediaApiApplication.java
@@ -1,4 +1,4 @@
-package com.azcona.socialmediaApi;
+package com.azcona.socialmediaApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
