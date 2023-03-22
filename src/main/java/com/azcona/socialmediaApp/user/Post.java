0e20bb52f0ca47f5dfac19aa93f7d5ed4e273abc
marcelo-azcona <marceloazcona86@gmail.com>
2023-03-22 19:37:44 -0300

    Created the Post entity

---
+++ src/main/java/com/azcona/socialmediaApp/user/Post.java
@@ -0,0 +1,40 @@
+package com.azcona.socialmediaApp.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class Post {
+
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	private String description;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	private User user;
+
+	public Post() {
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+}
